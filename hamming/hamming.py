2796e20402a2d06fa82cf2afd7c537f86c88c87a
MSoup <19704779+MSoup@users.noreply.github.com>
2020-01-05 00:13:56 +0900

    forgot a file

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise Exception("They must be the same length for comparison")
+    
+    hamming = 0
+    
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            hamming += 1
+    return hamming
