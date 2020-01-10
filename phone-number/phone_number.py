7d5b89c70f7877e696552b9efa81040e728a517f
MSoup <19704779+MSoup@users.noreply.github.com>
2020-01-10 13:43:10 +0900

    Update phone_number.py
    Added a few functions manually

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -3,14 +3,24 @@ class Box():
     self.number = self.getNumber(number)
 
   def getNumber(self, number):
+    #Make a blank list first, store all numbers in it
     blank = []
     for i in number:
       if i.isdigit():
         blank.append(i)
+    #Check for errors, but also omit leading 1 if no errors
+    if self.isValid(blank):
+      if blank[0] == '1':
+        return "".join(blank[1:])
+      #No errors, no leading 1, return whole number
+      return "".join(blank)
+  
+  def getAreaCode(self):
+    return self.number[:3]
 
-    if blank[0] == '1':
-      return "".join(blank[1:])
-    elif blank[0] == '0' or len(blank) != 10:
+  def isValid(self, lst):
+    if lst[0] == '0':
       raise ValueError("Invalid number")
-    
-    return "".join(blank)
+    elif len(lst) != 11 and len(lst) != 10:
+      raise ValueError("Invalid number")
+    return True
