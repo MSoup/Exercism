8009054995e2153846e7acec75b89288321e63a4
MSoup <19704779+MSoup@users.noreply.github.com>
2020-01-13 10:06:46 +0900

    now works

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,15 +1,39 @@
 class PhoneNumber:
-    def __init__(self, number):
-        self.rawnumber = "".append([i for i in number if i.isdigit()])
-        self.number = self.rawnumber[1:] if self.rawnumber[0] == '1' else self.rawnumber
+  def __init__(self, number):
+    self.number = self.getCleanNumber(number)
+    self.area_code = self.number[:3]
+    self.exchange_code = self.number[3:6]
+    self.rest = self.number[6:]
 
-        
-    def clean(self):
-        cleanSlate = ""
-        for i in self.number:
-            if i.isdigit():
-                cleanSlate += str(i)
-        if cleanSlate[0] == '1':
-            return cleanSlate[1:]
-        
-        return cleanSlate
+  def getCleanNumber(self, number):
+    cleaned = ""
+    for i in number:
+      if i.isdigit():
+        cleaned += i
+    
+    #Check first digit
+    if cleaned[0] == '1':
+      if self.checkValidity(cleaned[1:]):
+        return cleaned[1:]
+    
+    else:
+      if self.checkValidity(cleaned):
+        return cleaned
+
+  
+  def checkValidity(self, number):
+    #Is area code + exchange code + the rest 10 digits?
+    if len(number) != 10:
+      raise ValueError('Invalid number')
+    #Valid area code?
+    elif number[0] in '01':
+      raise ValueError('Invalid number')
+    #Valid exchange code?
+    elif number[3] in '01':
+      raise ValueError('Invalid number')
+
+    return True
+
+
+  def pretty(self):
+    return f"({self.area_code}) {self.exchange_code}-{self.rest}"
