d2d5c28cdfd7833ba2e99368dd4522301f58f6d0
MSoup <charmanduh@hotmail.com>
2020-01-14 02:02:19 +0000

    finished series

--- series/series.py
+++ series/series.py
@@ -1,2 +1,18 @@
 def slices(series, length):
-    pass
+  lst = []
+  if length > len(series) or length < 1:
+    raise ValueError("Please choose an appropriate length")
+
+  for i in range(len(series) - length + 1):
+    lst.append(series[i:i+length])
+
+  return lst
+
+
+
+print(slices("918493904243", 5))
+
+
+#["91849", "18493", "84939", "49390", "93904", "39042", "90424", "04243"],
+
+#["91", "14", "42"])
