67dc5ce5f6192e3834a819ba2157454fe4ac6e3b
MSoup <19704779+MSoup@users.noreply.github.com>
2020-01-19 18:31:50 +0900

    finished assignments

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,35 +1,11 @@
-"""
-Introduction
-Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+def square_of_sum(number):
+    return int(((number * (number + 1)) / 2)**2)
+    
+def sum_of_squares(number):
+    return int((number * (number + 1) * (2 * number + 1)) / 6)
 
-The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)² = 55² = 3025.
+def difference_of_squares(number):
+    return abs(square_of_sum(number) - sum_of_squares(number))
 
-The sum of the squares of the first ten natural numbers is 1² + 2² + ... + 10² = 385.
 
-Hence the difference between the square of the sum of the first ten natural numbers and the sum of the squares of the first ten natural numbers is 3025 - 385 = 2640.
-
-You are not expected to discover an efficient solution to this yourself from first principles; research is allowed, indeed, encouraged. Finding the best algorithm for the problem is a key skill in software engineering.
-"""
-
-def squareDiff(n):
-
-  """
-  #Inefficient way
-
-  sumSquares = 0
-  squareSum = 0
-  for i in range(1,n+1):
-    #Take squares first and add
-    sumSquares += i**2
-    #Take sum first, square outside the loop
-    squareSum += i
-  
-  squareSum = squareSum ** 2
-
-  return abs(sumSquares - squareSum)
-  """
-  #Thanks Carl Friedrich Gauss
-
-  return int(((n * (n + 1)) / 2)**2 - (n * (n + 1) * (2 * n + 1)) / 6)
-
-print(squareDiff(100))
+#Thanks Carl Friedrich Gauss
