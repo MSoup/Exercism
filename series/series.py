27041163cc24365e9ae58e30ab38a39fcc98ecdb
MSoup <charmanduh@hotmail.com>
2020-01-20 03:53:45 +0000

    finished series

--- series/series.py
+++ series/series.py
@@ -3,7 +3,7 @@ def slices(series, length):
   if length > len(series) or length < 1:
     raise ValueError("Please choose an appropriate length")
 
-  for i in range(len(series) - length + 1):
+  for i in range(len(str(series))-length+1):
     lst.append(series[i:i+length])
 
   return lst
