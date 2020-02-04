a86642066f94d7de112ae6022803a111c8540845
MSoup <charmanduh@hotmail.com>
2020-02-04 06:32:19 +0000

    finished anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,19 @@
 def find_anagrams(word, candidates):
-    pass
+  lst = []
+  
+  def is_anagram(w, e):
+    #Make sure w != e
+    w = w.lower()
+    e = e.lower()
+
+    if w == e:
+      return False
+    
+    #Make sure every occurance of element exists in word
+    return all([e.count(i) == w.count(i) for i in e])
+
+  for el in candidates:
+    if is_anagram(word, el):
+      lst.append(el)
+  
+  return lst
