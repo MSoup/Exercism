28e3c34f6302ea35e7ebb1fd4b679ef134b1dc6b
MSoup <charmanduh@hotmail.com>
2020-02-10 01:01:11 +0000

    finished word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,31 @@
+from string import punctuation
+import re
+
 def count_words(sentence):
-    pass
+  catalog = {}
+  #Split on any special character
+  words = re.split(r"[^A-Za-z0-9']", sentence)
+
+  #Filter words and build dictionary
+  punc = set(punctuation) - {"'"}
+
+  def cleaned(word):
+    #Remove punctuation except apostrophe
+    word = "".join([char for char in word if char not in punc])
+    #Remove leading / trailing apostrophe
+    word = word.strip(punctuation)
+    #Return word in lower case
+    return word.lower()
+
+  for word in words:
+    #Filter word
+    check_word = cleaned(word)
+    #Check against dict to build it; ignore empty check_word
+    if not check_word:
+      pass
+    elif check_word in catalog:
+      catalog[check_word] += 1
+    else:
+      catalog[check_word] = 1
+
+  return catalog
