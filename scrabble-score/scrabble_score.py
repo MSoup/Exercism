2af199ad99db2e4fd5e41756b4740e769ec8f510
MSoup <charmanduh@hotmail.com>
2020-03-02 23:32:37 +0000

    cleaner version of scrabble score

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,19 @@
 def score(word):
-    pass
+  score_map = {}
+  for letter in ("A, E, I, O, U, L, N, R, S, T").split(", "):
+    score_map[letter] = 1
+  for letter in ("D, G").split(", "):
+    score_map[letter] = 2
+  for letter in ("B, C, M, P").split(", "):
+    score_map[letter] = 3
+  for letter in ("F, H, V, W, Y").split(", "):
+    score_map[letter] = 4
+  
+  #Only one 5 point letter
+  score_map["K"] = 5
+  for letter in ("J, X").split(", "):
+    score_map[letter] = 8
+  for letter in ("Q, Z").split(", "):
+    score_map[letter] = 10
+  
+  return sum(score_map[char] for char in word.upper())
