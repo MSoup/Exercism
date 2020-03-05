136497d055e1023ce34052376e48b7c80e1a3c77
MSoup <charmanduh@hotmail.com>
2020-03-05 06:17:01 +0000

    finished saddle points

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,17 +1,35 @@
 def saddle_points(matrix):
-  matrix = [[4, 5, 4], 
-            [3, 5, 5], 
-            [1, 5, 4]]
+  for i in matrix:
+    if len(i) != len(matrix[0]):
+      raise ValueError("Irregular matrix")
 
-#For a saddle point to exist, it must be the highest number in its row, but also the lowest number in its compile
-  possible_saddle = []
-  for i, row in enumerate(matrix):
-    print(f'Looking at row {i+1}')
-    print(row)
-    biggest_row_num = max(row)
-    for col, el in enumerate(row):
-      print(f'Looking at column {col+1}: {el}')
-      if el == biggest_row_num:
-        print(f'{el} is the biggest in row {i+1}')
-        possible_saddle.append({"row":i+1, "column": col+1})
-  return print(possible_saddle)
+  #Store Candidates for Saddle Points
+  result_list = []
+  for row_count in range(len(matrix)):
+
+    row = matrix[row_count]
+    #max(row) is the number that will guarantee candidates since no other numbers will fulfill highest in row
+    row_candidate = max(row)
+    #Get index of all candidates
+    for i, num in enumerate(row):
+      #Highest num found
+      if num == row_candidate:
+        #Append index of num
+        result_list.append([row_count, i])
+
+  #True if a coordinate is less than or equal to all el in col
+  def test_candidate(row, col):
+    if all(matrix[row][col] <= i[col] for i in matrix):
+      return True
+    return False
+
+  results = []
+
+  for candidate in result_list:
+    #Send coordinates to be tested against matrix
+    #test_candidate: row, col
+    if test_candidate(candidate[0], candidate[1]):
+      #These are true saddle points, +1 to row/col index for human readable output
+      results.append({"row": candidate[0]+1, "column": candidate[1]+1})
+
+  return results
