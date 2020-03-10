d5e9ce3aae02598ab44d3dd0c1eb9c88876935c5
MSoup <charmanduh@hotmail.com>
2020-03-10 04:10:33 +0000

    final

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,3 @@
-#For a shape to be a triangle at all, all sides have to be of length > 0, and the sum of the lengths of any two sides must be greater than or equal to the length of the third side. See Triangle Inequality.
-
 def equilateral(sides):
   return check_inequality(sides) and check_type(sides) == 1
 
