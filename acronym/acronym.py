9ed27dc80f71aeda08f62e3790a5a75adc5129ff
MSoup <charmanduh@hotmail.com>
2020-03-31 03:52:30 +0000

    finished acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,8 @@
+import re
+
 def abbreviate(words):
-    pass
+  
+  chunked = re.split('[^a-zA-Z\']', words)
+  filtered = [i for i in chunked if i != ""]
+
+  return "".join(i.upper()[0] for i in filtered)
