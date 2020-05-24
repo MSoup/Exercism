60e5264a59d1b46a30a5b78717e308ad9c6abade
Daven <davenlu1989@gmail.com>
2020-05-24 20:34:54 +0900

    cleaned up code

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,18 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 class Garden:
-    def __init__(self, diagram, students = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]):
-        self.plantDict = {'R': "Radishes", 'C':"Clover", 'G':"Grass", 'V':"Violets"}
+
+    def __init__(self, diagram, students=[
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Fred',
+        'Ginny',
+        'Harriet',
+        'Ileana',
+        'Joseph',
+        'Kincaid',
+        'Larry',
+        ]):
+        self.plant_names = {
+            'R': 'Radishes',
+            'C': 'Clover',
+            'G': 'Grass',
+            'V': 'Violets',
+            }
         self.students = sorted(students)
 
-        #Generate clusters and store with each person
-        #for example: ['VVCCGGVV','CVAVCCGG]
         self.clusters = diagram.split('\n')
-        
-        #Generate dictionary of student:XXXX for each student, where X represents flower acronym (string) and student is a string
+
         self.belongings = {}
         for student in self.students:
-            studentPosition = self.students.index(student)*2
-            self.belongings[student] = self.clusters[0][studentPosition:studentPosition+2] + self.clusters[1][studentPosition:studentPosition+2]
-        
+            student_pos = self.students.index(student) * 2
+
+            # Get 4 letter clusters of plants
+
+            front_two = (self.clusters[0])[student_pos:student_pos + 2]
+            back_two = (self.clusters[1])[student_pos:student_pos + 2]
+            student_has = front_two + back_two
+
+            # Assign the flowers to each students
+
+            self.belongings[student] = [self.plant_names[letter]
+                    for letter in student_has]
+
     def plants(self, student):
-        #Iterate through already generated 4 alphabet bundles for called student
-        return [self.plantDict[flower] for flower in self.belongings[student]]
+        return self.belongings[student]
